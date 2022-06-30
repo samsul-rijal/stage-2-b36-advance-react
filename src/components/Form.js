059b9a2553668727e18317ac87de98c4255cebd3
Your Name <samsulr99@gmail.com>
2022-06-30 09:32:54 +0700

    update

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,35 @@
 // create style here
+const styles = {
+  form: {
+    margin: "16px"
+  },
+  formGroup: {
+    marginBottom: "16px"
+  },
+  formLabel: {
+    marginBottom: "8px",
+    display: "inline-block"
+  },
+  formInput: {
+    width: "100%",
+    padding: "12px",
+    fontSize: "15px",
+    color: "grey",
+    backgroundColor: "white",
+    borderRadius: "5px",
+    border: "1px solid grey"
+  }
+}
 
 // create Form component
 function Form() {
   return (
     // code inside div
-    <div>
-      {/* form */}
+    <div style={styles.form}>
+      <div style={styles.formGroup}>
+        <label htmlFor="inputUsername" style={styles.formLabel}>Username</label>
+        <input type="text" id="inputUsername" style={styles.formInput} placeholder="Isikan username" />
+      </div>
     </div>
   )
 }
